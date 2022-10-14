0be595469f5b60cca7d1dba6a306b8bff6984ff9
keviebokje <115719872+keviebokje@users.noreply.github.com>
2022-10-14 11:54:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,80 +1,77 @@
 //variabelen random numbers
 let computerNumber = Math.floor(Math.random() * 12) + 1;
 let playerNumber = Math.floor(Math.random() * 12) + 1;
-let userPoints = 100
+let userPoints = 100;
+const pn = document.querySelector('.playerN');
+const hoger = document.querySelector('.hoger');
+const lager = document.querySelector('.lager');
+const scoretekst = document.querySelector('.score')
+const buttonCpu = document.querySelector('.button1');
 
 //button cpu
 const cn = document.querySelector('.computerN');
 function randompc() {
-    document.querySelector('.hoger').disabled = false;
-    document.querySelector('.lager').disabled = false;
-    document.querySelector('.button1').disabled = true;
-    computerNumber = Math.floor(Math.random() * 12) + 1;
-    cn.textContent = computerNumber;
-    console.log(computerNumber);
+    if (userPoints > 0) {
+        document.querySelector('.hoger').disabled = false;
+        document.querySelector('.lager').disabled = false;
+        document.querySelector('.button1').disabled = true;
+        computerNumber = Math.floor(Math.random() * 12) + 1;
+        cn.textContent = computerNumber;
+        console.log(computerNumber);
+    } else (
+        scoretekst.textContent = 'game over'
+    )
 }
 
-const buttonCpu = document.querySelector('.button1');
 buttonCpu.addEventListener('click', randompc);
 //buttons higher lower aan
 document.querySelector('.button1').disabled = false;
 document.querySelector('.hoger').disabled = false;
 
-const pn = document.querySelector('.playerN');
 
-const hoger = document.querySelector('.hoger');
-
-const lager = document.querySelector('.lager');
-const scoretekst = document.querySelector('.score')
 //function button hoger knop gaan hier ook mee uit
 //kan alleen spelen als je punten hebt
 function randomPN() {
-    if (userPoints > 0) {
-        document.querySelector('.button1').disabled = false;
-        document.querySelector('.hoger').disabled = true;
-        document.querySelector('.lager').disabled = true;
-        let playerNumber = Math.floor(Math.random() * 12) + 1;
-        pn.textContent = playerNumber;
-        if (playerNumber > computerNumber) {
-            console.log('win')
-            scoretekst.textContent = 'you won'
-            userPoints = userPoints + 10
-            playerpoints.textContent = userPoints
-        }
-        else {
-            console.log('verloren')
-            scoretekst.textContent = 'you lost'
-            userPoints = userPoints - 10
-            playerpoints.textContent = userPoints
-        }
-    } else (
-        alert('je bent af')
-    )
+
+    document.querySelector('.button1').disabled = false;
+    document.querySelector('.hoger').disabled = true;
+    document.querySelector('.lager').disabled = true;
+    let playerNumber = Math.floor(Math.random() * 12) + 1;
+    pn.textContent = playerNumber;
+    if (playerNumber > computerNumber) {
+        console.log('win')
+        scoretekst.textContent = 'you won'
+        userPoints = userPoints + 10
+        playerpoints.textContent = userPoints
+    }
+    else {
+        console.log('verloren')
+        scoretekst.textContent = 'you lost'
+        userPoints = userPoints - 10
+        playerpoints.textContent = userPoints
+    }
 }
 //function button lager knop gaan hier ook mee uit
 //kan alleen spelen als je punten hebt
 function randompn2() {
-    if (userPoints > 0) {
-        document.querySelector('.lager').disabled = true;
-        document.querySelector('.button1').disabled = false;
-        document.querySelector('.hoger').disabled = true;
-        let playerNumber = Math.floor(Math.random() * 12) + 1;
-        pn.textContent = playerNumber;
-        if (playerNumber < computerNumber) {
-            console.log('win')
-            scoretekst.textContent = 'you won'
-            userPoints = userPoints + 10
-            playerpoints.textContent = userPoints
-        }
-        else {
-            console.log('verloren')
-            scoretekst.textContent = 'you lost'
-            userPoints = userPoints - 10
-            playerpoints.textContent = userPoints
-        }
-    } else (
-        alert('je bent af')
-    )
+    document.querySelector('.lager').disabled = true;
+    document.querySelector('.button1').disabled = false;
+    document.querySelector('.hoger').disabled = true;
+    let playerNumber = Math.floor(Math.random() * 12) + 1;
+    pn.textContent = playerNumber;
+    if (playerNumber < computerNumber) {
+        console.log('win')
+        scoretekst.textContent = 'you won'
+        userPoints = userPoints + 10
+        playerpoints.textContent = userPoints
+    }
+    else {
+        console.log('verloren')
+        scoretekst.textContent = 'you lost'
+        userPoints = userPoints - 10
+        playerpoints.textContent = userPoints
+    }
+
 }
 
 function reset() {
